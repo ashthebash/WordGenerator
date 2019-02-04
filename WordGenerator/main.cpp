3ce1fb3f966a3ae0b908505ef4c8d9715f877824
Ashley Walton <7148133+DarkRising101@users.noreply.github.com>
2019-02-04 22:37:46 +0000

    + added txt file to multidimensional vector + added random word generation from multidimensional vector using mersenne_twister_engine

--- WordGenerator/main.cpp
+++ WordGenerator/main.cpp
@@ -5,10 +5,38 @@
 
 using namespace std;
 
-int OrganiseWords(int minLength, int maxLength)
+vector<vector <string> > OrganiseWords(vector<vector <string> > words, int numberOfLetters)
 {
+	ifstream file("isogram.txt");
+	string line;
+	int wordIndex = 0;
+	vector<string> wordColumn;
 
-	return 0;
+	while (getline(file, line))
+	{
+		if (line.length() == numberOfLetters)
+		{
+			wordColumn.push_back(line);
+			cout << wordColumn[wordIndex] << endl;
+			wordIndex++;
+		}
+	}
+	words.push_back(wordColumn);
+	return words;
+}
+
+int WordListLength()
+{
+	ifstream file("isogram.txt");
+	string line;
+	int numberOfLines = 0;
+
+	while (getline(file, line))
+	{
+		numberOfLines++;
+	}
+
+	return numberOfLines;
 }
 
 int GetWordLength(int minLength,int maxLength)
@@ -46,20 +74,14 @@ int RandomNumber(int maxNumber)
 	return randRange(randNumber);
 }
 
-string GenerateWord(int wordLength)
+string GenerateWord(int wordLength, int minLength, vector<vector <string> > words)
 {
-	string newWord = "hello";
-	string line;
-	ifstream isograms("isogram.txt");
-	int numberOfLines = 0;
+	string newWord = "";
+	int wordColumn = wordLength - minLength;
+	int wordRowLength = words[wordColumn].size();
+	int pickword = RandomNumber(wordRowLength);
 
-	while (getline(isograms, line))
-	{
-		numberOfLines++;
-	}
-
-	int pickLine = RandomNumber(numberOfLines);
-	cout << pickLine << "\n";
+	newWord = words[wordColumn][pickword];
 	
 	return newWord;
 }
@@ -67,16 +89,25 @@ string GenerateWord(int wordLength)
 int main()
 {
 	const int minLength = 4, maxLength = 15;
+	vector< vector<string> > words;
+	int numberOfLines = WordListLength();
+	int numberOfLetters = minLength;
+	int wordRow = 0;
 
 	//Organise words
-	OrganiseWords(minLength, maxLength);
+	for (int i = 0; i < maxLength - minLength + 1; i++)
+	{
+		words = OrganiseWords(words, numberOfLetters);
+		wordRow++;
+		numberOfLetters++;
+	}
 
 	//Get word length
 	const int wordLength = GetWordLength(minLength,maxLength);
 
 	//generate word.
 	string word = "";
-	word = GenerateWord(wordLength);
+	word = GenerateWord(wordLength, minLength, words);
 	cout << "The word is: " << word << endl;
 
 	return 0;
